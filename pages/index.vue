6eb95c9b957b4d87f2058dd8e8ba6a61ea9cfe11
Shababul Alam <sababul37@gmail.com>
2022-08-10 10:11:10 +0600

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="min-h-screen flex flex-col justify-center py-12 bg-gray-50 sm:px-6 lg:px-8">
+      <div class="border max-w-xs mx-auto h-64 w-64 flex items-center justify-center rounded-xl bg-white shadow">
+          <h2 class="text-4xl font-black">Hello</h2>
+      </div>
+  </div>
+</template>
